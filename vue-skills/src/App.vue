c67018e7a84f53b20b3610615dfc338ce7571516
Jerry Thimothy J <jerrythimothy@gmail.com>
2018-09-03 21:10:56 +0530

    Vue 2 learning

---
+++ vue-skills/src/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <div id="app">
+    <nav>
+      <router-link to="/">Home</router-link>
+      <router-link to="/about">About</router-link>
+    </nav>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+  import Skills from './components/Skills.vue'
+  
+  export default {
+    name: 'app',
+    components: {
+      Skills
+    }
+  }
+</script>
+
+<style>
+  @import url('https://fonts.googleapis.com/css?family=Montserrat:400,700');
+  body {
+    background-color: #EEEEEE;
+    font-family: 'Montserrat', sans-serif;
+    display: grid;
+    grid-template-rows: auto;
+    justify-items: center;
+    padding-top: 50px;
+  }
+  
+  nav {
+    padding: 20px 20px 20px 0;
+  }
+  
+  nav a {
+    padding: 10px;
+    text-decoration: none;
+    background: #fff;
+    border-radius: 3px;
+    color: rgb(0, 110, 255);
+    font-weight: bold;
+    margin-right: 15px;
+  }
+  
+  body,
+  html {
+    margin: 0;
+    height: 100%;
+  }
+  
+  #app {
+    width: 50%;
+  }
+</style>
